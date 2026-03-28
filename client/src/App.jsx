913eb09ac71a1e7ea4c44f7f178c6800077ee999
Saikat20040103 <saikat03012004@gmail.com>
2026-03-28 18:51:50 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './pages/Home' 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
 
 function App() {
-
-  return <Home/>
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
